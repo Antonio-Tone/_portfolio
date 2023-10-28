3d2b5f1c5c48fc854850d7b78ff0f4e5f75b51e8
Antonio-Tone <Antoniotone815@gmail.com>
2023-10-28 14:42:46 +0200

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -71,6 +71,95 @@
       </div>
      </div>
     
+     <div class="container mb-5 m-0">
+    <div class="wrapper">
+      <div class="row row-1 mb-3">
+        <div class="col-3">
+           <div class="flex-col">
+            <div class="d-flex justify-content-center">
+            <img class="profile" src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg" alt="picture">
+           </div>
+           <div class="text-center">
+            <p class="test">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis et facilis fugit repellat! Voluptatibus, dignissimos tempore dolorum ipsam deleniti optio mollitia minima aspernatur porro quis error sit veniam unde?</p>
+           </div>
+           </div>
+        </div>
+        <div class="col-3">
+          <div class="flex-col">
+            <div class="d-flex justify-content-center">
+            <img class="profile" src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg" alt="picture">
+           </div>
+           <div class="text-center">
+            <p class="test">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis et facilis fugit repellat! Voluptatibus, dignissimos tempore dolorum ipsam deleniti optio mollitia minima aspernatur porro quis error sit veniam unde?</p>
+           </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="flex-col">
+            <div class="d-flex justify-content-center">
+            <img class="profile" src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg" alt="picture">
+           </div>
+           <div class="text-center">
+            <p class="test">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis et facilis fugit repellat! Voluptatibus, dignissimos tempore dolorum ipsam deleniti optio mollitia minima aspernatur porro quis error sit veniam unde?</p>
+           </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="flex-col">
+            <div class="d-flex justify-content-center">
+            <img class="profile" src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg" alt="picture">
+           </div>
+           <div class="text-center">
+            <p class="test">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis et facilis fugit repellat! Voluptatibus, dignissimos tempore dolorum ipsam deleniti optio mollitia minima aspernatur porro quis error sit veniam unde?</p>
+           </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row row-2">
+        <div class="col-3">
+          <div class="flex-col">
+            <div class="d-flex justify-content-center">
+            <img class="profile" src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg" alt="picture">
+           </div>
+           <div class="text-center">
+            <p class="test">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis et facilis fugit repellat! Voluptatibus, dignissimos tempore dolorum ipsam deleniti optio mollitia minima aspernatur porro quis error sit veniam unde?</p>
+           </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="flex-col">
+            <div class="d-flex justify-content-center">
+            <img class="profile" src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg" alt="picture">
+           </div>
+           <div class="text-center">
+            <p class="test">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis et facilis fugit repellat! Voluptatibus, dignissimos tempore dolorum ipsam deleniti optio mollitia minima aspernatur porro quis error sit veniam unde?</p>
+           </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="flex-col">
+            <div class="d-flex justify-content-center">
+            <img class="profile" src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg" alt="picture">
+           </div>
+           <div class="text-center">
+            <p class="test">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis et facilis fugit repellat! Voluptatibus, dignissimos tempore dolorum ipsam deleniti optio mollitia minima aspernatur porro quis error sit veniam unde?</p>
+           </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="flex-col">
+            <div class="d-flex justify-content-center">
+            <img class="profile" src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg" alt="picture">
+           </div>
+           <div class="text-center">
+            <p class="test">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet reiciendis et facilis fugit repellat! Voluptatibus, dignissimos tempore dolorum ipsam deleniti optio mollitia minima aspernatur porro quis error sit veniam unde?</p>
+           </div>
+          </div>
+        </div>
+      </div>
+    </div>
+     </div>
 
     </div>
 </template>
@@ -150,4 +239,32 @@ left: 50%;
   /* transform: translate(50%,-50%);} */
   
 }
+.profile{
+  height: 90px;
+  width: 90px;
+  border-radius:999px
+}
+.test{
+  font-size: small;
+}
+.container{
+  min-width: 100% !important;
+  background-color: lightcoral;
+  padding: 23px;
+}
+.wrapper{
+  padding: 20px;
+background-color: greenyellow;
+}
+.flex-col{
+  background-color: burlywood;
+  padding: 20px;
+ height: 250px;
+ width: 300px;
+}
+.col-4{
+  /* background-color: grey; */
+ 
+}
+
 </style>
