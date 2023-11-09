c563a262ef09a5d1ff783d22cbeac3a38142c113
Antonio-Tone <Antoniotone815@gmail.com>
2023-11-09 09:11:55 +0200

    changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,49 @@
 <template>
   <div>
-    <h1 class="text-center quote mt-5 ">
-            <span
-              ><i class="fa-solid fa-quote-left fa-flip-vertical" id="me"></i></span
-            >Celebrate your unique story, for it is the brushstroke that paints <br> the canvas of your remarkable life<span
-              ><i class="fa-solid fa-quote-right fa-flip-vertical"></i></span>
-          </h1>
+    <div class="qoute mb-5">
+      <h1 class="text-center quote mt-5">
+      <span
+        ><i class="fa-solid fa-quote-left fa-flip-vertical"></i></span
+      >Celebrate your unique story, for it is the brushstroke that paints <br />
+      the canvas of your remarkable life<span>
+        <i class="fa-solid fa-quote-right fa-flip-vertical"></i> </span>
+    </h1>
+    </div>
+
+    <div class="d-flex justify-content-center">
+      <hr class="hr">
+      <a href="/contact"><magnetic class="switch" buttonText="Contact" /></a>
+    </div>
+
   </div>
 </template>
 <script>
+import magnetic from "../components/magButtonComp.vue";
 export default {
-  
-}
+  components:{
+    magnetic 
+  }
+};
 </script>
 <style>
-  h1{
-   color: white !important  ;
-  }
-  i{
-    font-size: 20px;
-    transform: translateY(-15px) !important;
-  }
-  #me{
-    transform: rotateX('90') !important;
-  }
+h1 {
+  color: white !important  ;
+}
+i {
+  font-size: 20px;
+  transform: translateY(-15px) !important;
+}
+.hr{
+  position: relative;
+  background-color: white !important;
+  /* height: 1px; */
+  width: 50%;
+}
+.switch{
+  position: absolute;
+  transform: translateY(-35px) translateX(-130px);
+  z-index: 3;
+}
+
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -343,9 +343,9 @@
               <div class="text-center">
                 <p class="name mx-auto">Luke Evertson</p>
                 <p class="test autotype">
-                  Antonio is a very fun person to work with and has a great
+                  "Antonio is a very fun person to work with and has a great
                   sense of humour.He Is hardworking and disciplined.He could be
-                  a role model for many
+                  a role model for many."
                 </p>
               </div>
             </div>
@@ -362,10 +362,10 @@
               <div class="text-center">
                 <p class="name mx-auto">Taahirah Ismail</p>
                 <p class="test autotype">
-                  He may be a man of few words, but Antonio's hard work and
+                  "He may be a man of few words, but Antonio's hard work and
                   dedication always shine through in whatever he does. You can
                   easily tell he appreciates every opportunity he is given and
-                  takes nothing for granted
+                  takes nothing for granted."
                 </p>
               </div>
             </div>
@@ -382,11 +382,11 @@
               <div class="text-center">
                 <p class="name mx-auto">Asiphe Ndimlana</p>
                 <p class="test autotype">
-                  Antonio is very independent. Whenever he struggles with
+                  "Antonio is very independent. Whenever he struggles with
                   something he would rather first try and figure it out himself
                   before asking for help. He is extremely kind and friendly and
                   turns every person he meets for the first time into his
-                  friend.
+                  friend."
                 </p>
               </div>
             </div>
@@ -403,10 +403,10 @@
               <div class="text-center">
                 <p class="name mx-auto">Ethan Oliver</p>
                 <p class="test autotype">
-                  I am thrilled to share my positive testimonial about my
+                  "I am thrilled to share my positive testimonial about my
                   experience with Antonio. From the moment I first met him I was
                   impressed. His dedication to excellence and drive to achieve
-                  was evident in every interaction I had with him.
+                  was evident in every interaction I had with him."
                 </p>
               </div>
             </div>
@@ -425,10 +425,10 @@
               <div class="text-center">
                 <p class="name mx-auto">Deno Rautenbach</p>
                 <p class="test autotype">
-                  Antonio is a very hardworking individual.He has improved a lot
+                 " Antonio is a very hardworking individual.He has improved a lot
                   in both his coding aswell as in his problem solving skills. He
                   is an extremely nice person to work with as he is very
-                  friendly
+                  friendly"
                 </p>
               </div>
             </div>
@@ -445,7 +445,7 @@
               <div class="text-center">
                 <p class="name mx-auto">Ayesha Galant</p>
                 <p class="test autotype">
-                  Working alongside Antonio on several IT ventures has been an absolute pleasure. His precision-oriented approach coupled with his knack for troubleshooting is highly commendable. He unequivocally embodies talent and commitment in his work ethic.
+                 "Working alongside Antonio on several IT ventures has been an absolute pleasure. His precision-oriented approach coupled with his knack for troubleshooting is highly commendable. He unequivocally embodies talent and commitment in his work ethic."
                 </p>
               </div>
             </div>
@@ -462,10 +462,10 @@
               <div class="text-center">
                 <p class="name mx-auto">Maxwill Isaacs</p>
                 <p class="test autotype">
-                  Antonio is an exceptionally hard worker and always making time
+                  "Antonio is an exceptionally hard worker and always making time
                   to assist anyone who is in need. Also goes the extra-mile when
                   doing anything that has to be done. He is also very confident
-                  in his capabilities and never fails to get the Job done.
+                  in his capabilities and never fails to get the Job done"
                 </p>
               </div>
             </div>
@@ -482,7 +482,7 @@
               <div class="text-center">
                 <p class="name mx-auto">Laiquah Cotie</p>
                 <p class="test autotype">
-                Antonio  is an exceptional professional, with deep expertise, dedication, and strong problem-solving skills. His work ethic, clear communication, and collaborative nature make him a remarkable team player. I wholeheartedly recommend him.
+                "Antonio  is an exceptional professional, with deep expertise, dedication, and strong problem-solving skills. His work ethic, clear communication, and collaborative nature make him a remarkable team player. I wholeheartedly recommend him."
                 </p>
               </div>
             </div>
@@ -499,7 +499,7 @@
               <div class="text-center">
                 <p class="name mx-auto">Ganiefah Majiet</p>
                 <p class="test autotype">
-                  Antonio one, someone who Id definitely recommend. His always positive and willing to help the next person although he himself is busy, his an intelligent and very hardworking person. He is always positive and he always provides everyone with help and motivation.
+                  "Antonio one, someone who Id definitely recommend. His always positive and willing to help the next person although he himself is busy, his an intelligent and very hardworking person. He is always positive and he always provides everyone with help and motivation"
                 </p>
               </div>
             </div>
